853e8d7d3409212edfcadda834ea6499ba249c1b
freeton20 <doctor4891@gmail.com>
2021-10-30 21:11:34 +0300

    utils.surfCodeHash

--- src/api/surfTransactionsVolumes.js
+++ src/api/surfTransactionsVolumes.js
@@ -16,7 +16,7 @@ function makeQuery() {
                 }
                 account:{
                 code_hash:{
-                    eq:"207dc560c5956de1a2c1479356f8f3ee70a59767db2bf4788b1d61ad42cdad82"
+                    eq: "${utils.surfCodeHash}"
                 }
                 }
                 }
@@ -56,7 +56,6 @@ const surfTransactionsVolumes = async () => {
     }
 }
 
-//surfTransactionsVolume();
 module.exports = {
     surfTransactionsVolumes
 }

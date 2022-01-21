d5a53ba77d93219c4893ce5756d9c2152078d233
freeton20 <doctor4891@gmail.com>
2022-01-21 22:10:35 +0200

    rocket, menu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <b-navbar  style="padding-left:3rem" toggleable="lg" type="dark" variant="success">
+      <b-navbar-brand href="#"  v-b-toggle.sidebar-1>☰</b-navbar-brand>      
       <b-navbar-brand href="#"  v-b-toggle.sidebar-1>Everscale Mainnet</b-navbar-brand>      
     </b-navbar>
    <router-view class="mt-4"/>   
-
+   <Rocket/>
     <Sidebar/>
   </div>
 </template>
 <script>
 import Sidebar from './components/sidebar/sidebar.vue'
+import Rocket from './components/rocket.vue'
 export default {
   components:{
-    Sidebar
+    Sidebar,
+    Rocket
   }
 }
 </script>
